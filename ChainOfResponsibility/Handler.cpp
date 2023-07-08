b3675387eee85a27a9829ad68be5de67f6820323
sangingeum <56298463+sangingeum@users.noreply.github.com>
2023-07-08 19:07:01 +0900

    Delegate requests only when unable to handle them
    Delegate requests only when unable to handle them

--- ChainOfResponsibility/Handler.cpp
+++ ChainOfResponsibility/Handler.cpp
@@ -4,16 +4,17 @@ void R1Handler::handleRequest(const Request& request) {
 	if (request.getType() == RequestType::R1) {
 		std::cout << "R1 Handler handled a request\n Content: " + request.getContent() << "\n";
 	}
-	if (m_nextHandler) {
+	else if (m_nextHandler) {
 		m_nextHandler->handleRequest(request);
 	}
+	
 }
 
 void R2Handler::handleRequest(const Request& request) {
 	if (request.getType() == RequestType::R2) {
 		std::cout << "R2 Handler handled a request\n Content: " + request.getContent() << "\n";
 	}
-	if (m_nextHandler) {
+	else if (m_nextHandler) {
 		m_nextHandler->handleRequest(request);
 	}
 }
@@ -22,7 +23,7 @@ void R3Handler::handleRequest(const Request& request) {
 	if (request.getType() == RequestType::R3) {
 		std::cout << "R3 Handler handled a request\n Content: " + request.getContent() << "\n";
 	}
-	if (m_nextHandler) {
+	else if (m_nextHandler) {
 		m_nextHandler->handleRequest(request);
 	}
 }
