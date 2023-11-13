f1499f26d4a7badb044e88275bfc76ae113b1da8
Sangin Geum <56298463+sangingeum@users.noreply.github.com>
2023-11-14 01:49:26 +0900

    Add 'fallback' example
    Add 'fallback' example

--- MiscPatterns/BehaviorTree/main.cpp
+++ MiscPatterns/BehaviorTree/main.cpp
@@ -5,7 +5,7 @@
 #include "Decorator.hpp"
 #include <iostream>
 
-int main() {
+void testSequence() {
 	auto printA = std::make_unique<Leaf>([]() {std::cout << "A\n"; return Node::State::Success; });
 	auto printB = std::make_unique<Leaf>([]() {std::cout << "B\n"; return Node::State::Failure; });
 	auto printC = std::make_unique<Leaf>([]() {std::cout << "C\n"; return Node::State::Failure; });
@@ -17,7 +17,6 @@ int main() {
 	sequence->addChild(std::move(printC));
 	sequence->addChild(std::move(printD));
 
-
 	BehaviorTree tree{ std::move(sequence) };
 	tree.tick(); // A
 	tree.tick(); // B
@@ -28,6 +27,33 @@ int main() {
 	tree.tick(); // C
 
 	// D is not triggered because C returns a failure
+}
 
+void testFallBack() {
+	auto printA = std::make_unique<Leaf>([]() {std::cout << "A\n"; return Node::State::Failure; });
+	auto printB = std::make_unique<Leaf>([]() {std::cout << "B\n"; return Node::State::Success; });
+	auto printC = std::make_unique<Leaf>([]() {std::cout << "C\n"; return Node::State::Success; });
+	auto printD = std::make_unique<Leaf>([]() {std::cout << "D\n"; return Node::State::Failure; });
+	auto invertedB = std::make_unique<Decorator>(std::move(printB));
+	auto fallback = std::make_unique<Fallback>();
+	fallback->addChild(std::move(printA));
+	fallback->addChild(std::move(invertedB));
+	fallback->addChild(std::move(printC));
+	fallback->addChild(std::move(printD));
 
+	BehaviorTree tree{ std::move(fallback) };
+	tree.tick(); // A
+	tree.tick(); // B
+	tree.tick(); // C
+
+	tree.tick(); // A
+	tree.tick(); // B
+	tree.tick(); // C
+
+	// D is not triggered because C returns a success
+}
+
+int main() {
+	testSequence();
+	testFallBack();
 }
